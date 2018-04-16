a3ad8e0df50f0ced08bb76eea45a16c9b8f07b1e
Henrik Nilsson <henrik.n@live.se>
2018-04-16 10:39:27 +0200

    Initial Commit

---
+++ TheDude/TheDude/Source.cpp
@@ -0,0 +1,51 @@
+#ifdef _DEBUG
+#pragma comment(lib, "LuaLibd.lib")
+#else
+#pragma comment(lib, "Lualib.lib")
+#endif
+
+#include <lua.hpp>
+#include <SFML\Graphics.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include "lua.h"
+
+void ConsoleThread(lua_State* L) {
+	char command[1000];
+	while (GetConsoleWindow()) {
+		memset(command, 0, 1000);
+		std::cin.getline(command, 1000);
+		if (!strcmp(command, "exit"))
+			break;
+		if (luaL_loadstring(L, command) || lua_pcall(L, 0, 0, 0))
+			std::cout << lua_tostring(L, -1) << '\n';
+	}
+}
+
+int main()
+{
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	std::thread conThread(ConsoleThread, L);
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	conThread.join();
+	return 0;
+}
